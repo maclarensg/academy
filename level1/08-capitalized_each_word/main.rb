eac67b556636be0e4eeb98e50ef03746b1a0a291
Gavin <maclarensg>
2020-06-23 20:11:26 +0800

    save

--- level1/08-capitalized_each_word/main.rb
+++
@@ -1,7 +0,0 @@
-def capitalize(str)
-  str.split(" ").map { |e| e.capitalize }.join(" ")
-end
-
-p capitalize("a short sentence")
-p capitalize("a lazy fox")
-p capitalize("look, it is working!")
--- level1/12-findthevowels/main.rb
+++
@@ -1,8 +0,0 @@
-def vowels(s)
-  s.downcase.split("").count{ |e|  ["a","e","i","o","u"].include? e }
-end
-
-
-p vowels("Hi There!")
-p vowels("Why do you ask?")
-p vowels("Why?")
